2556776295cbc5e3b6d3a020a9f7f048ba32a042
Adam Reed <adam@adamjreed.com>
2016-03-13 13:33:53 -0400

    - Abstracted validating periods into the base controller class. - Added top songs API.

--- api/app/Http/routes.php
+++ api/app/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 // API ROUTES ==================================
 Route::group(array('prefix' => 'v1'), function() {
+    /** latest */
     Route::get('latest', 'LatestController@index');
+
+    /** songs */
+    Route::get('songs', 'SongController@index');
 });
