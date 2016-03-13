635fb264019abacdce2d4e996c83b3304b6e537e
Adam Reed <adam@adamjreed.com>
2016-03-13 13:43:23 -0400

    Frontend for top songs

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -36,6 +36,18 @@ function Latest($http, API_URL) {
     }
 };
 angular
+    .module('app.songs')
+    .factory('Songs',  ['$http', 'API_URL', Songs]);
+
+function Songs($http, API_URL) {
+    return {
+        // get the top songs
+        get : function(period) {
+            return $http.jsonp(API_URL + 'v1/songs?period=' + period + "&callback=JSON_CALLBACK");
+        }
+    }
+};
+angular
     .module('app.tabs')
     .directive('navigationTabs', navigationTabs);
 
@@ -93,8 +105,31 @@ angular
     .module('app.songs')
     .controller('SongsController', SongsController);
 
-function SongsController($scope) {
+function SongsController($scope, Songs) {
+    $scope.loading = true;
     $scope.activeTab = 'songs';
+    $scope.currentPeriod = '24 hours';
+    $scope.periods = [
+        {id: '24 hours', name: "24 hours"},
+        {id: '1 week', name: "1 week"},
+        {id: '1 month', name: "1 month"},
+        {id: '1 year', name: "1 year"}
+    ];
+
+    $scope.loadTopSongs = function() {
+        Songs.get($scope.currentPeriod)
+            .success(function(data) {
+                $scope.songs = data.response;
+                $scope.loading = false;
+            });
+    };
+
+    $scope.changePeriod = function() {
+        $scope.loading = true;
+        $scope.loadTopSongs();
+    };
+
+    $scope.loadTopSongs();
 };
 angular
     .module('app')
--- app/public/modules/songs/songs.controller.js
+++ app/public/modules/songs/songs.controller.js
@@ -2,6 +2,29 @@ angular
     .module('app.songs')
     .controller('SongsController', SongsController);
 
-function SongsController($scope) {
+function SongsController($scope, Songs) {
+    $scope.loading = true;
     $scope.activeTab = 'songs';
+    $scope.currentPeriod = '24 hours';
+    $scope.periods = [
+        {id: '24 hours', name: "24 hours"},
+        {id: '1 week', name: "1 week"},
+        {id: '1 month', name: "1 month"},
+        {id: '1 year', name: "1 year"}
+    ];
+
+    $scope.loadTopSongs = function() {
+        Songs.get($scope.currentPeriod)
+            .success(function(data) {
+                $scope.songs = data.response;
+                $scope.loading = false;
+            });
+    };
+
+    $scope.changePeriod = function() {
+        $scope.loading = true;
+        $scope.loadTopSongs();
+    };
+
+    $scope.loadTopSongs();
 };
